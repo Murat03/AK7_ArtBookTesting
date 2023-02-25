7d4c086701291dfbe92b295b2c676c8f87da1c69
Murat03 <muratipekk033@gmail.com>
2023-02-25 17:12:56 +0300

    New Project With Testing Implementations

---
+++ app/src/main/java/com/muratipek/ak7_artbooktesting/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.muratipek.ak7_artbooktesting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
